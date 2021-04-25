c56741bb56e02920fcacb23b488e50c93a500e9f
Jim Charnesky <mijnesky@hotmail.com>
2021-04-24 19:21:26 -0600

    Start tests

--- test/user-test.js
+++ test/user-test.js
@@ -13,12 +13,6 @@ import User from "../User.js";
 // };
 
 
-// each trip is an obj
-//
-//
-//
-
-
 describe('Traveler', () => {
   let user1, user2;
 
@@ -27,14 +21,26 @@ describe('Traveler', () => {
   });
 });
 
+  it('User should be a function', () => {
+
+  })
+
+
+  it('Should be able to show me all of my trips', () => {
+    expect().to.equal();
+});
+
 
 // iteration 1
 // should be able to show me all of my trips
 //  A: filter method will get my trips return trip.userID === user.id
+
 // should be able to tell me if a trip is past, present, upcomming, or pending
 //  A: forEach method or map method trip and compare trip.date to current date (moment.js library) if trip.date < currentDate return 'past' ... (repeat for others)
+
 // should be able to return the total amount spent on triops in a year + 10%
 //  A: use getAllTrips method to .reduce iteratie through trips to get costs and add 10%
+
 // const myTrips = getMyTrips() - this is the filter method from line 32;
 // return myTrips.reduce((total, trip) => {
 //   total += trip.cost + trip.cost * .10;
@@ -56,10 +62,3 @@ describe('Traveler', () => {
 // makesure the id is good
 // will send fetch call to endpoint to get information for a single traveler (or more for travel agent)
 // remeber, the login has a conditonal pathway for 2 different dashboards
-
-
-describe('Should create a user', function() {
-  it('should return true', function() {
-    expect(true).to.equal(true);
-  });
-});
